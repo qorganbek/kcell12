9e4169857814803191a653e0e4565ef792334421
qorganbek <dimashqorganbek@gmail.com>
2023-06-20 21:18:33 +0600

    upd

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,26 +1,9 @@
 from typing import List
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import (path, include, URLPattern)
-
 from core import views
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny, ],
-)
 
 
 urlpatterns: List[URLPattern] = [
